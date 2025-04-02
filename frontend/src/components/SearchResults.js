763ab26d4df1a7b32a0ce50d62f69994e5f44bf4
kachup1 <emailkarlahere@gmail.com>
2025-04-02 16:45:54 -0700

    Added frames

--- frontend/src/components/SearchResults.js
+++ frontend/src/components/SearchResults.js
@@ -1,8 +1,33 @@
 import React from "react";
-import "./SearchResults.module.css";
+import styles from "./SearchResults.module.css";
 
-function SearchResults() {
-    return <div>Search Results Component</div>;
+const SearchResults = () => {
+    return (
+        <div className={styles.resultsContainer}>
+            <div className={styles.mainFrame}>
+            <h4>main Frame</h4>
+                <div className={styles.topMainFrame}> 
+                    <div className={styles.topFrame1}>
+                        <div className={styles.leftSquare}></div>
+                            {/*Vehicle Title*/}
+                            <div className={styles.vehicleTitle}>Camry LE</div>
+                        <div className={styles.rightSquare}></div>
+                    </div>
+                    <div className={styles.topFrame2}>
+                        
+                        {/*Problem Header */}
+                        <div className={styles.problemHeader}>AC Filter</div>
+                        {/*Link for user to add their quote */}
+                        <div className={styles.addYourQuoteHeader}>Want to share your quotes? Add yours!</div>
+                    </div>
+                    <div className={styles.resultInfoFrame}>
+                    <h4>ressult Info Frame</h4>
+                    </div>
+                </div>
+            </div>
+            <p>Search Results Component</p>
+        </div>
+    );
 }
 
-export default SearchResults; 
+export default SearchResults;
