6b72e4be99ef5042696e1ece8521b57196ed8fc3
Montse58 <montsealonso2001@gmail.com>
2025-04-03 20:55:02 -0700

    Changed the files to modules and modified code to be compatible with styles

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,14 +1,15 @@
 import React from "react";
-import "./Homepage.css";
+import "./Homepage.module.css";
 import TopMenu from "./TopMenu"; 
 import { Link } from "react-router-dom";
+import styles from "./Homepage.module.css";
 
 const Homepage = () =>{
 
 return (
-    <div className="homepage">
-      <h1>Welcome to Carqos</h1>
-      <div className="menu">
+    <div className={styles["homepage"]}>
+    
+      <div className={styles["menu"]}>
         <TopMenu/>
       </div>
       
