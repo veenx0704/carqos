97bdefe2ae38891d8ff6852258f2b8eaf2bdbd11
Montse58 <montsealonso2001@gmail.com>
2025-04-04 00:11:08 -0700

    added popular searches, need to still put in the specifics

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -12,6 +12,35 @@ return (
       <div className={styles["menu"]}>
         <TopMenu/>
       </div>
+      <div className={styles["popular-search"]}>
+        Popular searches
+      </div >
+      <div className={styles["popular-search-details"]}>
+        <div className={styles["popular-search-1"]}>
+          <div className={styles["popular-search-img1"]}>
+
+          </div>
+
+        </div>
+        <div className={styles["popular-search-2"]}>
+        <div className={styles["popular-search-img2"]}>
+            
+            </div>
+            <div className={styles["popular-search-car-name"]}>
+
+            </div>
+          
+        </div>
+        <div className={styles["popular-search-3"]}>
+        <div className={styles["popular-search-img3"]}>
+            
+            </div>
+          
+        </div>
+
+      </div>
+      
+
       
     </div>
   );
