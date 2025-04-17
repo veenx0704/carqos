37095e383209715cd704cb09401333f928564d72
Montse58 <montsealonso2001@gmail.com>
2025-04-16 17:18:00 -0700

    Added popular search section, still need to adjust the sizing, placing

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -12,37 +12,155 @@ return (
       <div className={styles["menu"]}>
         <TopMenu/>
       </div>
-      <div className={styles["popular-search"]}>
-        Popular searches
-      </div >
-      <div className={styles["popular-search-details"]}>
-        <div className={styles["popular-search-1"]}>
-          <div className={styles["popular-search-img1"]}>
 
+      
+    {/*Popular Searches */}
+      <div className={styles["popular-search"]}>
+        Popular Searches
+      </div >
+      
+
+      <div className={styles["popular-search-details"]}>
+
+        {/*This is the 1st option */}
+        <div className={styles["popular-search-1"]}>
+          <div className={styles["popular-search-car-name"]}>
+          Silverado 2500 HD
           </div>
 
-        </div>
-        <div className={styles["popular-search-2"]}>
-        <div className={styles["popular-search-img2"]}>
-            
+          <div className={styles["popular-search-make-year"]}>
+            <div className={styles["popular-search-make"]}>
+            Chevrolet
             </div>
+            <div className={styles["popular-search-year"]}>
+            2024
+            </div>
+            </div>
+
+            <div className={styles["popular-search-issue-list"]}>
+            Brake Pad
+            Battery
+            Transmission
+           
+            </div>
+
+        </div>
+
+        {/*This is the 2nd option */}
+        <div className={styles["popular-search-2"]}>
             <div className={styles["popular-search-car-name"]}>
+              Silverado EV
+            </div>
+            <div className={styles["popular-search-make-year"]}>
+            <div className={styles["popular-search-make"]}>
+            Chevrolet
+            </div>
+            <div className={styles["popular-search-year"]}>
+            2024
+            </div>
+            </div>
+
+            <div className={styles["popular-search-issue-list"]}>
 
             </div>
           
         </div>
+
+        {/*This is the 3rd option */}
         <div className={styles["popular-search-3"]}>
-        <div className={styles["popular-search-img3"]}>
-            
+            <div className={styles["popular-search-car-name"]}>
+            Camry SE
+            </div>
+
+            <div className={styles["popular-search-make-year"]}>
+            <div className={styles["popular-search-make"]}>
+            Toyota
+            </div>
+            <div className={styles["popular-search-year"]}>
+            2024
+            </div>
+            </div>
+
+            <div className={styles["popular-search-issue-list"]}>
+
             </div>
           
         </div>
+
+        {/*This is the 4th option */}
+        <div className={styles["popular-search-4"]}>
+            <div className={styles["popular-search-car-name"]}>
+              Equinox
+            </div>
+            <div className={styles["popular-search-make-year"]}>
+            <div className={styles["popular-search-make"]}>
+            Chevrolet
+            </div>
+            <div className={styles["popular-search-year"]}>
+            2024
+            </div>
+            </div>
+
+            <div className={styles["popular-search-issue-list"]}>
+
+            </div>
+          
+        </div>
+
+
+        {/* This is the 5th option*/}
+        <div className={styles["popular-search-5"]}>
+            <div className={styles["popular-search-car-name"]}>
+            Trailblazer
+            </div>
+
+            <div className={styles["popular-search-make-year"]}>
+            <div className={styles["popular-search-make"]}>
+            Chevrolet
+            </div>
+            <div className={styles["popular-search-year"]}>
+            2024
+            </div>
+            </div>
+
+            <div className={styles["popular-search-issue-list"]}>
+
+            </div>
+          
+        </div>
+
+        {/*This is the 6th option */}
+        <div className={styles["popular-search-6"]}>
+            <div className={styles["popular-search-car-name"]}>
+            Corolla
+            </div>
+
+            <div className={styles["popular-search-make-year"]}>
+            <div className={styles["popular-search-make"]}>
+              Toyota
+
+            </div>
+            <div className={styles["popular-search-year"]}>
+            2024
+            </div>
+            </div>
+
+            <div className={styles["popular-search-issue-list"]}>
+
+            </div>
+          
+        </div>
+        
 
       </div>
       
 
+
+      
+
       
     </div>
+    
   );
 };
 
